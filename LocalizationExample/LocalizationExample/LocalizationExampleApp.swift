e1f60c1d5d4cd35e946932626f3be91fcf686950
Ceren <cerengunnes@gmail.com>
2024-09-29 14:10:56 +0300

    Add base project.

---
+++ LocalizationExample/LocalizationExample/LocalizationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationExampleApp.swift
+//  LocalizationExample
+//
+//  Created by Ceren Güneş on 27.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct LocalizationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
